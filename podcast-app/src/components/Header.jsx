49896ca9bb25b7aaedfc2aa7eda76c41813b01c5
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-07-05 13:57:09 +0200

    Update header and hamburger icon display

--- podcast-app/src/components/Header.jsx
+++ podcast-app/src/components/Header.jsx
@@ -1,5 +1,10 @@
 import { Link, NavLink } from "react-router-dom"
+import { useState
+
+ } from "react";
 export default function Header() {
+
+    const [menuOpen, setMenuOpen] = useState(false);
 
      const activeStyles = {
         fontWeight: "bold",
@@ -7,14 +12,20 @@ export default function Header() {
         color: "#2DD4BF"
     }
 
+    const handleLinkClick = () => {
+        setMenuOpen(false); // auto-close on link click
+    };
+
     return (
         <header>
-                <Link className="site-link" to="/"><img className="site-logo" src='../assets/images/u-tune_logo_3.png'/></Link>
-                <nav>
-                    <NavLink to="/favouriteshows"  style={ ({isActive}) => isActive ? activeStyles : null }>❤️ Favourites</NavLink>
-                    <NavLink to="/shows" end style={ ({isActive}) => isActive ? activeStyles : null }>🌐 Explore</NavLink>
-                    <NavLink to="/search" style={ ({isActive}) => isActive? activeStyles : null }>🔍 Search</NavLink>
-                    <Link to="login" className="login-link">
+                <Link className="site-link" to="/"><img className="site-logo" src='../assets/images/u-tune_logo_1.png' alt="U-Tune Logo"/></Link>
+                <Link className="site-title" to="/"><img className="site-title" src='../assets/images/u-tune_logo_2.png' alt="U-Tune Title"/></Link>
+                <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>{menuOpen ? '×' : '☰'}</button>
+                <nav className={menuOpen ? 'open' : ''}>
+                    <NavLink to="/favouriteshows"  style={ ({isActive}) => isActive ? activeStyles : null } onClick={handleLinkClick}>❤️ Favourites</NavLink>
+                    <NavLink to="/shows" end style={ ({isActive}) => isActive ? activeStyles : null } onClick={handleLinkClick}>🌐 Explore</NavLink>
+                    <NavLink to="/search" style={ ({isActive}) => isActive? activeStyles : null } onClick={handleLinkClick}>🔍 Search</NavLink>
+                    <Link to="login" className="login-link" onClick={handleLinkClick}>
                         <img src="../assets/images/avatar-icon.png" className="login-icon"/>
                     </Link>
                 </nav> 
