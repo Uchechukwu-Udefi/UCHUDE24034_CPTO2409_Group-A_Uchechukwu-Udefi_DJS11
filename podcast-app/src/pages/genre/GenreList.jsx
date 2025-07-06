68e8b9cc0c08c47c6836d015e2817aa6b11f9c07
Uchechukwu-Udefi <udefihenry@gmail.com>
2025-07-06 12:03:02 +0200

    Update genre list styling

--- podcast-app/src/pages/genre/GenreList.jsx
+++ podcast-app/src/pages/genre/GenreList.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 
 export default function GenreList() {
   return (
-    <div className="genre-container">
-    <h1>Explore by Genres</h1>
-    <div className="genre-list">
-        {Object.entries(genreMap).map(([id, name]) => (
-            <Link to={`/genre/${id}`} key={id} className="genre-list-link">{name}</Link>
-        ))}
-    </div>
-    </div>
+   <div className="genre-container">
+  <h1>Explore by Genres</h1>
+  <div className="genre-list">
+    {Object.entries(genreMap).map(([id, name]) => (
+      <Link to={`/genre/${id}`} key={id} className="genre-list-link">
+        {name}
+      </Link>
+    ))}
+  </div>
+</div>
+
   );
 }
